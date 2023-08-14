21d34f31ecec15b186f0aa4f10c20477d3557a1e
Soloobas <solomonobadjere@gmail.com>
2023-08-14 07:59:14 -0700

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - prints the alphabet in reverse order
+ * main - Prints numbers from 0 to 9.
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    int i;
+	int i;
 
-    for (i = 122; i >= 97; i--)
-    {
-        putchar(i);
-    }
-    putchar('\n');
-    return (0);
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
 }
