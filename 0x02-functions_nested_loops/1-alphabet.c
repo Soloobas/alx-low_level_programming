133b419e8349036c81e635e2d7aae62701f7c45d
Soloobas <solomonobadjere@gmail.com>
2023-08-15 16:52:44 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,11 +3,11 @@
 /**
  * print_alphabet - print all alphabet in lowercase
  */
-
 void print_alphabet(void)
 {
-	char letter; 
+	char letter;
+
 	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter); 
+		_putchar(letter);
 	_putchar('\n');
-}	
+}
