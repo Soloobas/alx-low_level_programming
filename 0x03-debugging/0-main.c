29a882a6e670694ca453c764ec25bf5d2c1ae9c5
Soloobas <solomonobadjere@gmail.com>
2023-08-16 04:47:51 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -6,18 +6,34 @@
  * @n: The number to be checked
  * Return: void
  */
+
 void positive_or_negative(int n)
 {
-    if (n > 0)
-    {
-        printf("%d is positive\n", n);
-    }
-    else if (n < 0)
-    {
-        printf("%d is negative\n", n);
-    }
-    else
-    {
-        printf("%d is zero\n", n);
-    }
+	if (n > 0)
+    	{
+        	printf("%d is positive\n", n);
+    	}
+    	else if (n < 0)
+    	{
+        	printf("%d is negative\n", n);
+    	}
+    	else
+    	{
+		printf("%d is zero\n", n);
+    	}
+}
+
+/**
+ * main - Test function for positive_or_negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+    	i = 0;
+    	positive_or_negative(i);
+
+    	return (0);
 }
